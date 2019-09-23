e7c648671e374ed972f4fa1c0e47652394c2171e
n013301 <xiaohui.duan@archermind.com>
2019-09-23 15:56:01 +0800

    chore : add
    		added resource of fileViewNoteWidget

--- application/pdfControl/fileViewNote/FileViewNoteWidget.cpp
+++ application/pdfControl/fileViewNote/FileViewNoteWidget.cpp
@@ -4,6 +4,8 @@ FileViewNoteWidget::FileViewNoteWidget(CustomWidget *parent):
     CustomWidget(QString("FileViewNoteWidget"), parent)
 {
     resize(300, 400);
+
+    initWidget();
 }
 
 int FileViewNoteWidget::dealWithData(const int &, const QString &)
@@ -14,11 +16,30 @@ int FileViewNoteWidget::dealWithData(const int &, const QString &)
 
 void FileViewNoteWidget::initWidget()
 {
-    m_pTextEdit = new QTextEdit;
+    m_pHLayoutClose = new QHBoxLayout;
+    m_pHLayoutDlt = new QHBoxLayout;
+    m_pTextEdit = new DTextEdit;
     m_pVLayout = new QVBoxLayout;
-    m_pVLayout->setContentsMargins(1, 1, 1, 1);
-    m_pVLayout->setSpacing(1);
+    m_pCloseLab = new DLabel;
+    m_pDltLab = new DLabel;
+
+    m_pHLayoutClose->setContentsMargins(1, 0, 0, 0);
+    m_pCloseLab->setFixedSize(QSize(50, 50));
+    m_pCloseLab->setPixmap(QString(":/resources/image/close.svg"));
+    m_pHLayoutClose->addSpacing(1);
+    m_pHLayoutClose->addWidget(m_pCloseLab);
+
+    m_pHLayoutDlt->setContentsMargins(0, 0, 1, 0);
+    m_pDltLab->setFixedSize(QSize(50, 50));
+    m_pCloseLab->setPixmap(QString(":/resources/image/delete.svg"));
+    m_pHLayoutDlt->addWidget(m_pDltLab);
+    m_pHLayoutDlt->addSpacing(1);
+
+    m_pVLayout->setContentsMargins(0, 0, 0, 0);
+    m_pVLayout->setSpacing(0);
+    m_pVLayout->addItem(m_pHLayoutClose);
     m_pVLayout->addWidget(m_pTextEdit);
+    m_pVLayout->addItem(m_pHLayoutDlt);
 
     this->setLayout(m_pVLayout);
 }
--- application/pdfControl/fileViewNote/FileViewNoteWidget.h
+++ application/pdfControl/fileViewNote/FileViewNoteWidget.h
@@ -1,14 +1,17 @@
 #ifndef FILEVIEWNOTEWIDGET_H
 #define FILEVIEWNOTEWIDGET_H
 
-//#include <DWidget>
-#include "subjectObserver/CustomWidget.h"
+#include <DLabel>
+#include <DTextEdit>
 
 #include <QVBoxLayout>
-#include <QTextEdit>
+#include <QHBoxLayout>
+
+#include "subjectObserver/CustomWidget.h"
 
 class FileViewNoteWidget : public CustomWidget
 {
+    Q_OBJECT
 public:
     FileViewNoteWidget(CustomWidget *parent = nullptr);
 
@@ -20,8 +23,12 @@ protected:
     void initWidget() override;
 
 private:
-    QTextEdit *m_pTextEdit = nullptr;
+    DTextEdit *m_pTextEdit = nullptr;
     QVBoxLayout *m_pVLayout = nullptr;
+    QHBoxLayout *m_pHLayoutClose = nullptr;
+    QHBoxLayout *m_pHLayoutDlt = nullptr;
+    DLabel *m_pCloseLab = nullptr;
+    DLabel *m_pDltLab = nullptr;
 };
 
 #endif // FILEVIEWNOTEWIDGET_H
