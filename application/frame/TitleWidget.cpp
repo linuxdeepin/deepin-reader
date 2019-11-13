d994fcec7b10f29b521d5ed4111d4473acc9a0e6
yunzhen.kong <yunzhen.kong@deepin.io>
2019-11-13 18:39:32 +0800

    chore :solve conflict
    solve conflict

--- application/frame/TitleWidget.cpp
+++ application/frame/TitleWidget.cpp
@@ -165,7 +165,8 @@ void TitleWidget::slotActionTrigger(QAction *action)
     }
 
     if (nCurrentState != m_nCurHandleShape) {
-        m_nCurHandleShape = nCurrentState;   
+        m_nCurHandleShape = nCurrentState;
+        QString ss = tr("111");
         QString normalPic = PF::getImagePath(btnName, Pri::g_frame);
         m_pHandleShapeBtn->setIcon(QIcon(normalPic));
 
