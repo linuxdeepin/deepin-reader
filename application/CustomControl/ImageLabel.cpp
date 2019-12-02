5da62261ccd020e723c6ffab0f568658527265a4
n013301 <xiaohui.duan@archermind.com>
2019-12-02 17:10:37 +0800

    chore : UI
    		adjused UI of add line width of big thumbnail

--- application/CustomControl/ImageLabel.cpp
+++ application/CustomControl/ImageLabel.cpp
@@ -1,12 +1,13 @@
 #include "ImageLabel.h"
-#include "utils/utils.h"
-#include <QPainter>
-#include <QPalette>
 #include <DGuiApplicationHelper>
 #include <QDebug>
+#include <QPainter>
+#include <QPalette>
+#include "controller/DBManager.h"
+#include "utils/utils.h"
 
 ImageLabel::ImageLabel(DWidget *parent)
-    : DLabel (parent)
+    : DLabel(parent)
 {
     setAttribute(Qt::WA_TranslucentBackground);
 }
@@ -14,34 +15,49 @@ ImageLabel::ImageLabel(DWidget *parent)
 void ImageLabel::setSelect(const bool &select)
 {
     m_bSelect = select;
+    if (m_bSelect) {
+        if (m_nRadius == 8) {
+            m_nHighLightLineWidth = 4;
+        } else if (m_nRadius == 10) {
+            m_nHighLightLineWidth = 8;
+        }
+    }
     update();
 }
 
 void ImageLabel::paintEvent(QPaintEvent *e)
 {
+    DLabel::paintEvent(e);
+
     int local = 0;
     int width = this->width();
     int heigh = this->height();
-    int penwidth=0;
+    int penwidth = 0;
     QPalette p(this->palette());
     QPainter painter(this);
     painter.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
+
+    penwidth = m_nHighLightLineWidth;
 
     if (m_bSelect) {
         local = 1;
         width -= 2;
         heigh -= 2;
-        penwidth=5;
-        painter.setPen(QPen(DGuiApplicationHelper::instance()->applicationPalette().highlight().color(), penwidth, Qt::SolidLine));
+        //        penwidth = 8;
+        painter.setPen(
+            QPen(DGuiApplicationHelper::instance()->applicationPalette().highlight().color(),
+                 penwidth, Qt::SolidLine));
     } else {
         local = 2;
         width -= 4;
         heigh -= 4;
-        penwidth=1;
-        painter.setPen(QPen(DGuiApplicationHelper::instance()->applicationPalette().frameShadowBorder().color(), penwidth, Qt::SolidLine));
+        penwidth = 1;
+        painter.setPen(QPen(
+            DGuiApplicationHelper::instance()->applicationPalette().frameShadowBorder().color(),
+            penwidth, Qt::SolidLine));
     }
 
-    if(m_bSetBp){
+    if (m_bSetBp) {
         //填充图片
         QPainterPath path;
         QRectF pixrectangle(local, local, width, heigh);
@@ -53,9 +69,7 @@ void ImageLabel::paintEvent(QPaintEvent *e)
         painter.drawRoundedRect(rectangle, m_nRadius, m_nRadius);
     }
 
-    DLabel::paintEvent(e);
-    if(m_bshowbookmark)
-    {
+    if (m_bshowbookmark) {
         QString ssPath = ":/resources/image/";
         DGuiApplicationHelper::ColorType themeType = DGuiApplicationHelper::instance()->themeType();
         if (themeType == DGuiApplicationHelper::LightType)
@@ -64,9 +78,9 @@ void ImageLabel::paintEvent(QPaintEvent *e)
             ssPath += "dark";
 
         ssPath += "/checked/bookmarkbig_checked_light.svg";
-        QPixmap pixmap(Utils::renderSVG(ssPath,/*QSize(16,16)*/QSize(36,36)));
+        QPixmap pixmap(Utils::renderSVG(ssPath, /*QSize(16,16)*/ QSize(36, 36)));
         QPainter painter1(this);
         painter1.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
-        painter1.drawPixmap(this->width()-36-6,0, 36,36, pixmap);
+        painter1.drawPixmap(this->width() - 36 - 6, 0, 36, 36, pixmap);
     }
 }
--- application/CustomControl/ImageLabel.h
+++ application/CustomControl/ImageLabel.h
@@ -1,8 +1,8 @@
 #ifndef IMAGELABEL_H
 #define IMAGELABEL_H
 
-#include <DWidget>
 #include <DLabel>
+#include <DWidget>
 
 DWIDGET_USE_NAMESPACE
 
@@ -16,14 +16,8 @@ public:
 
 public:
     void setSelect(const bool &select);
-    inline void setRadius(const int radius)
-    {
-        m_nRadius = radius;
-    }
-    inline void setBookMarkStatus(bool bshow)
-    {
-        m_bshowbookmark = bshow;
-    }
+    inline void setRadius(const int radius) { m_nRadius = radius; }
+    inline void setBookMarkStatus(bool bshow) { m_bshowbookmark = bshow; }
 
     inline void setBackgroundPix(QPixmap pixmap)
     {
@@ -32,15 +26,16 @@ public:
     }
 
 protected:
-    void  paintEvent(QPaintEvent *e) Q_DECL_OVERRIDE;
+    void paintEvent(QPaintEvent *e) Q_DECL_OVERRIDE;
 
 private:
     bool m_bSelect = false;
-    int m_nRadius = 8;         // 圆角
-    bool m_bshowbookmark = false;     //显示书签
+    int m_nRadius = 8;             // 圆角
+    bool m_bshowbookmark = false;  //显示书签
 
-    QPixmap m_background;        // 缩略图
-    bool m_bSetBp = false;       // 是否设置缩略图
+    QPixmap m_background;           // 缩略图
+    bool m_bSetBp = false;          // 是否设置缩略图
+    int m_nHighLightLineWidth = 0;  // 高亮边框宽度
 };
 
-#endif // IMAGELABEL_H
+#endif  // IMAGELABEL_H
