541d1ce6b45175c441f0a63e1c805a6cef0058d0
leiyu <leiyu@uniontech.com>
2020-05-13 18:42:16 +0800

    Type:chore
    Description:remove unless code
    
    RootCause:Null
    
    Solution:

--- application/lpreviewControl/SheetSidebar.cpp
+++ application/lpreviewControl/SheetSidebar.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     leiyu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "SheetSidebar.h"
 #include "DocSheet.h"
 #include "thumbnail/ThumbnailWidget.h"
@@ -246,16 +265,6 @@ void SheetSidebar::dealWithPressKey(const QString &sKey)
     } else if (sKey == KeyStr::g_del) {
         deleteItemByKey();
     }
-}
-
-void SheetSidebar::handleLeftPressKey()
-{
-    onJumpToPrevPage();
-}
-
-void SheetSidebar::handleRightPressKey()
-{
-    onJumpToNextPage();
 }
 
 void SheetSidebar::onJumpToPrevPage()
--- application/lpreviewControl/SheetSidebar.h
+++ application/lpreviewControl/SheetSidebar.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019 ~ 2019 Deepin Technology Co., Ltd.
+ *
+ * Author:     leiyu
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef LEFTSIDEBARWIDGET_H
 #define LEFTSIDEBARWIDGET_H
 
@@ -39,8 +58,6 @@ public :
     ~SheetSidebar() override;
 
     void setBookMark(int page, int state);
-    void handleLeftPressKey();
-    void handleRightPressKey();
     void handleOpenSuccess();
     void handleFindOperation(int);
     void handleFindContentComming(const stSearchRes &);
