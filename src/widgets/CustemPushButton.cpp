40b160cac3e7aa7328c705620d46d0553755bec6
xiaohui.duan <duanxiaohui@uniontech.com>
2020-08-25 14:08:43 +0800

    chore: 修改版权信息
    修改版权信息
    
    Log: 修改版权信息
    
    link:

--- src/widgets/CustemPushButton.cpp
+++ src/widgets/CustemPushButton.cpp
@@ -3,6 +3,8 @@
  *
  * Author:     duanxiaohui<duanxiaohui@uniontech.com>
  *
+ * Maintainer: duanxiaohui<duanxiaohui@uniontech.com>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
