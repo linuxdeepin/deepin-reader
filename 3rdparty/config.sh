337bcd4f16817e1590f32e67c1b0bab145b14c78
zhangsong <zhangsong@uniontech.com>
2020-10-14 14:00:52 +0800

    chore: 更新deepin-pdfium
    更新deepin-pdfium
    
    Log: 更新deepin-pdfium
    
    link:

--- 3rdparty/config.sh
+++ 3rdparty/config.sh
@@ -4,7 +4,7 @@ mkdir deepin-pdfium
 cd deepin-pdfium
 git init
 git remote add origin https://gitee.com/pigzhang/deepin-pdfium.git
-git fetch origin 2c49f7169bd828672200a25974c3e4ddcb72c461
+git fetch origin d4f33e767d9b921ad6e2de45ac0eb4b91afdaa91
 git reset --hard FETCH_HEAD
 qmake
 make -j16
