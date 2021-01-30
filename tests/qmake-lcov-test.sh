e407756222ec61d6d0db6b98fe091ac61e3cd530
wangzhixuan <wangzhixuan@uniontech.com>
2021-01-30 10:03:49 +0800

    chore: 更新 覆盖率统计, 采用正向方式,统一移除目录
     更新 覆盖率统计, 采用正向方式,统一移除目录
    
    Log: 更新 覆盖率统计, 采用正向方式,统一移除目录
    Change-Id: I8d5a3c17f5dc867a1c4b8c4cefebe07761f76c89

--- tests/qmake-lcov-test.sh
+++ tests/qmake-lcov-test.sh
@@ -10,19 +10,14 @@ result_coverage_dir=$build_dir/coverage
 
 result_report_dir=$build_dir/report/report.xml
 
-#下面是覆盖率目录操作，采用正向操作，
-extract_info="*/reader/*"  #针对当前目录进行覆盖率操作
-
 $build_dir/$executable --gtest_output=xml:$result_report_dir
 
 lcov -d $build_dir -c -o $build_dir/coverage.info
 
-lcov --extract $build_dir/coverage.info $extract_info --output-file  $build_dir/coverage.info
+lcov --extract $build_dir/coverage.info '*/reader/*' -o  $build_dir/coverage.info
 
-lcov --list-full-path -e $build_dir/coverage.info –o $build_dir/coverage-stripped.info
+lcov --remove $build_dir/coverage.info '*/tests/*' -o  $build_dir/coverage.info
 
 genhtml -o $result_coverage_dir $build_dir/coverage.info
- 
-lcov -d $build_dir –z
 
 exit 0
