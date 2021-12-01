314396bd47869404db1351f555cddb76ab49c18c
chendu <chendu@uniontech.com>
2021-12-01 16:09:57 +0800

    fix: 1030环境打的1050包在104X镜像上存在标签页样式的bug
    更改所需接口的dtk依赖版本
    
    Log: 修复1030环境打的1050包在104X镜像上存在标签页样式的bug
    Change-Id: I94a2756f6fe57358f9e31496bd8ef43e9b93dca8

--- reader/uiframe/DocTabBar.cpp
+++ reader/uiframe/DocTabBar.cpp
@@ -35,11 +35,8 @@
 DocTabBar::DocTabBar(QWidget *parent)
     : DTabBar(parent)
 {
-#if (DTK_VERSION_MAJOR > 5 || ((DTK_VERSION_MAJOR == 5 && DTK_VERSION_MINOR > 4) || (DTK_VERSION_MAJOR == 5 && DTK_VERSION_MINOR == 4 && DTK_VERSION_PATCH >=10)))
-    this->setEnabledEmbedStyle(true);//设置直角样式
-#endif
-
 #if (DTK_VERSION_MAJOR > 5 || (DTK_VERSION_MAJOR >= 5 && DTK_VERSION_MINOR >= 2 ))
+    this->setEnabledEmbedStyle(true);//设置直角样式
     this->setExpanding(true);//设置平铺窗口模式
 #endif
 
