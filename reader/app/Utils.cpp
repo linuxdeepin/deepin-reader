bb8f3e921e4b3efe47f185b28164df3ef6f12613
fengli <fengli@uniontech.com>
2023-10-24 14:45:50 +0800

    fix: 无法复制
    未加载到库文件时，不进行复制拦截
    
    Log: 修复无法复制文本问题
    
    Bug: https://pms.uniontech.com/bug-view-218701.html

--- reader/app/Utils.cpp
+++ reader/app/Utils.cpp
@@ -94,13 +94,15 @@ QPixmap Utils::roundQPixmap(const QPixmap &img_in, int radius)
 
 void  Utils::copyText(const QString &sText)
 {
-    int intercept = -1;
+    int intercept = 0;
     if(getLoadLibsInstance()->m_document_clip_copy){
         qInfo() << "当前文档: *** "/* <<m_currenFilePath*/;
         getLoadLibsInstance()->m_document_clip_copy(m_currenFilePath.toLocal8Bit().data(),&intercept);
         qInfo() << "是否拦截不允许复制(1:拦截 0:不拦截): " <<intercept;
     }
-    if(intercept) return;
+    if(intercept > 0)
+        return;
+
     QClipboard *clipboard = DApplication::clipboard();
     QString sOldText = clipboard->text(QClipboard::Clipboard);
     if (sOldText != sText) {
