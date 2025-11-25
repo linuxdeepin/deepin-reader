ddd6868821535b562cdb404efa8ad134d98dfe0c
ZhangTingan <ZhangTingan@uniontech.com>
2025-11-25 15:16:54 +0800

    Revert "chore: Remove unused source"
    This reverts commit 59e7d747e52ef30fadde487338e0f208f02eb12f.

---
+++ 3rdparty/deepin-pdfium/src/3rdparty/pdfium/fx_skia.cmake
@@ -0,0 +1,8 @@
+target_sources(pdfium
+    PRIVATE
+        # Headers
+        pdfium/third_party/skia_shared/SkFloatToDecimal.h
+
+        # Source files
+        pdfium/third_party/skia_shared/SkFloatToDecimal.cpp
+) 
