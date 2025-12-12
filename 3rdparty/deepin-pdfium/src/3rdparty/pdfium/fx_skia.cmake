4d3b57d1ea01ff77339ae4091e324cc8a09c3dfe
Hillwood Yang <hillwood@opensuse.org>
2025-12-12 17:06:47 +0800

    fix: Drop useless deepin-pdfium code
    Please package deepin-pdfium separately.
    
    Log: Refer https://github.com/linuxdeepin/deepin-pdfium

--- 3rdparty/deepin-pdfium/src/3rdparty/pdfium/fx_skia.cmake
+++
@@ -1,8 +0,0 @@
-target_sources(pdfium
-    PRIVATE
-        # Headers
-        pdfium/third_party/skia_shared/SkFloatToDecimal.h
-
-        # Source files
-        pdfium/third_party/skia_shared/SkFloatToDecimal.cpp
-) 
