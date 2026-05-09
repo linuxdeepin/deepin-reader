47fffc9b308ca726343a445b08581bfde58bc3db
gongheng <gongheng@uniontech.com>
2026-05-09 15:07:32 +0800

    Fix: add translate for short cut.
    Add translate for short cut.
    
    Log: fix issue
    Bug: https://pms.uniontech.com/bug-view-354843.html

---
+++ lupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2026 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader.ts
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader_zh_CN.ts
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader_zh_TW.ts
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader_zh_HK.ts
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader_ug.ts
+lupdate ./reader -ts -no-obsolete ./translations/deepin-reader_bo.ts
